b540df2a04b14dc9922b51d770f44b0d5b8f51dc
yamahitomi <yama@asnica.co.jp>
2020-04-02 18:33:41 +0900

    ログイン機能作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def new 
   end
-  
+
   def create
     user = User.find_by(name: params[:session][:name])
     if user && user.authenticate(params[:session][:password])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+  validate :name_presence
+  validate :password_presence
+  validates :name, uniqueness: { message: "そのユーザーは既に存在します"}
+
+  has_secure_password
+
+  private
+
+  def name_presence
+    return if name.present?
+    errors.add(:base, "名前が未入力です")
+  end
+  def password_presence
+    return if password.present? && password_confirmation.present?
+    errors.add(:base, "パスワードが未入力です")
+  end
 end
